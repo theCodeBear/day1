17fa0967e10bc00f61607d5d8872626c1bdb24b9
Todd Kronenberg <toddkronenberg@gmail.com>
2014-10-27 17:16:07 -0700

    Implement the randomize button
    Also make the adding to list work on Enter press

--- js/random.js
+++ js/random.js
@@ -1,16 +1,28 @@
 $(function(){
 
-    var button = $("button");
-    console.log(button);
 
-
-    button.on("click", function() {
-        console.log("button was just clicked");
-        var newItem = $("input").val();
-        if (newItem != "") {
-            $("ul").append($("<li>").text(newItem));    // can create an html tag in jQuery using $("<tag>")
+// Handle Enter press on add item input field
+    $("input[name=value_to_add]").on("keypress", function (e) {
+        if (e.keyCode == 13) {    // 13 is the Enter key
+            var $this = $(this);
+            var newItem = $this.val();
+            if (newItem != "") {
+                $("ul").append($("<li>").text(newItem));    // can create an html tag in jQuery using $("<tag>")
+            }
+            $this.val("");
         }
     });
+
+// Handle Pick Random Item button click event
+    $("#randomize").on("click", function() {
+        var itemList = $("li");
+        var randomIndex = Math.floor(Math.random() * (itemList.length));
+        var randomItem = itemList[randomIndex];
+
+        //debugger;       // debugger stops the program here so that you can debug the program
+        $("#selected_random").text($(randomItem).text());   // wrap randomItem with jQuery selector
+    });
+
 
 });
 
